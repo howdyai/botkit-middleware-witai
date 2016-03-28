cfe84955aea6ef65f4155eb25914f69a5a893244
Ben Brown <ben@xoxco.com>
2016-03-28 15:00:47 -0500

    Add example bot and readme file

---
+++ example_bot.js
@@ -0,0 +1,89 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+           ______     ______     ______   __  __     __     ______
+          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
+          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
+           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
+            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
+
+
+This is a sample Slack bot built with Botkit.
+
+This bot demonstrates many of the core features of Botkit:
+
+* Connect to Slack using the real time API
+* Receive messages based on "spoken" patterns
+* Reply to messages
+* Use the conversation system to ask questions
+* Use the built in storage system to store and retrieve information
+  for a user.
+
+# RUN THE BOT:
+
+  Get a Bot token from Slack:
+
+    -> http://my.slack.com/services/new/bot
+
+  Get a Wit token from Wit.ai
+
+    -> https://wit.ai/apps/new
+
+  Run your bot from the command line:
+
+    wit=<MY WIT TOKEN> token=<MY TOKEN> node bot.js
+
+# USE THE BOT:
+
+  Train a "hello" intent inside Wit.ai.  Give it a bunch of examples
+  of how someone might say "Hello" to your bot.
+
+  Find your bot inside Slack to send it a direct message.
+
+  Say: "Hello"
+
+  The bot should reply "Hello!" If it didn't, your intent hasn't been
+  properly trained - check out the wit.ai console!
+
+  Make sure to invite your bot into other channels using /invite @<my bot>!
+
+# EXTEND THE BOT:
+
+  Botkit is has many features for building cool and useful bots!
+
+  Read all about it here:
+
+    -> http://howdy.ai/botkit
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+if (!process.env.token) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+if (!process.env.wit) {
+    console.log('Error: Specify wit in environment');
+    process.exit(1);
+}
+
+var Botkit = require('botkit');
+var wit = require('botkit-middleware-witai')({
+    token: process.env.wit,
+});
+
+
+var controller = Botkit.slackbot({
+    debug: true,
+});
+
+var bot = controller.spawn({
+    token: process.env.token
+}).startRTM();
+
+controller.middleware.receive.use(wit.receive);
+
+
+/* note this uses example middlewares defined above */
+controller.hears(['hello'], 'direct_message,direct_mention,mention', wit.hears, function(bot, message) {
+    bot.reply(message, 'Hello!');
+});
---
+++ readme.md
@@ -0,0 +1,53 @@
+# Use Wit.ai's natural language tools in your Botkit-powered Bot!
+
+This middleware plugin for [Botkit](http://howdy.ai/botkit) allows you
+to seamlessly integrate Wit.ai natural language intent APIs into your Botkit bot.
+
+[Wit.ai](http://wit.ai) provides a service that uses machine learning to
+help developers handle natural language input.  The Wit API receives input
+from the user, and translates it into one or more "intents" which map to known
+actions or choices.  The power of Wit is that it can continually be trained
+to understand more and more responses without changing the underlying bot code!
+
+## Setup
+
+Create a Wit application [here](https://wit.ai/apps/new).  Then, set up and
+train at least one intent.
+
+From your app's settings page, snag the *Server Access Token*. You will
+need this to use Wit's API.
+
+Add botkit-middleware-witai as a dependency to your Botkit bot!
+
+```
+npm install --save botkit-middleware-witai`
+```
+
+Enable the middleware:
+
+```
+
+var wit = require('botkit-middleware-witai')({
+    token: <my_wit_token>
+});
+
+controller.middleware.receive.use(wit.receive);
+
+controller.hears(['hello'],'direct_message',wit.hears,function(bot, message) {
+
+    // ...
+});
+```
+
+For a full example [example_bot.js](example_bot.js)
+
+## What it does
+
+Using the Wit receive middleware with Botkit causes every message that is
+sent to your bot to be first sent to Wit.ai for processing. The
+results of the call to Wit.ai are added into the incoming message
+as `message.intents`, and will match the results of [this Wit.ai API call](https://wit.ai/docs/http/20141022#get-intent-via-text-link).
+
+Using the Wit hears middleware tells Botkit to look for Wit.ai intents
+information, and match using this information instead of the built in
+pattern matching function.
