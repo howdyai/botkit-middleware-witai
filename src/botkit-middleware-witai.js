f6117c6adb66781540daa69d710a2764a3bef9a6
DFischer <me@danielfischer.com>
2016-05-05 22:39:49 -0700

    Update middleware to look for intent entity

--- src/botkit-middleware-witai.js
+++ src/botkit-middleware-witai.js
@@ -1,4 +1,9 @@
-var wit = require('node-wit');
+'use strict';
+const Logger = require('node-wit').Logger;
+const levels = require('node-wit').logLevels;
+const Wit = require('node-wit').Wit;
+
+const logger = new Logger(levels.DEBUG);
 
 module.exports = function(config) {
 
@@ -10,30 +15,29 @@ module.exports = function(config) {
         config.minimum_confidence = 0.5;
     }
 
-    var middleware = {};
+    const client = new Wit(config.token, actions, logger);
+
+    const middleware = {};
 
     middleware.receive = function(bot, message, next) {
         if (message.text) {
-            wit.captureTextIntent(config.token, message.text, function(err, res) {
-                if (err) {
-                    next(err);
+            client.message(message.text, (error, data) => {
+                if (error) {
+                    next(error);
                 } else {
-                    console.log(JSON.stringify(res));
-                    message.intents = res.outcomes;
+                    // no support for multiple outcomes, is it needed for this?
+                    message.entities = data.outcomes[0].entities;
                     next();
                 }
             });
         }
-
     };
 
     middleware.hears = function(tests, message) {
-
-        if (message.intents) {
-            for (var i = 0; i < message.intents.length; i++) {
+        if (message.entities.intent) {
+            for (var i = 0; i < message.entities.intent.length; i++) {
                 for (var t = 0; t < tests.length; t++) {
-                    if (message.intents[i].intent == tests[t] &&
-                        message.intents[i].confidence >= config.minimum_confidence) {
+                    if (message.entities.intent[i].value == tests[t]) {
                         return true;
                     }
                 }
