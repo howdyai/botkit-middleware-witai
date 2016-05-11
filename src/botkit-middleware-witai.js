9508d4ed10aba128b4b11fe97ac98710051efe63
Daniel Fischer <me@danielfischer.com>
2016-05-11 13:19:10 -0700

    Update botkit-middleware-witai.js
    Guard for entities

--- src/botkit-middleware-witai.js
+++ src/botkit-middleware-witai.js
@@ -47,7 +47,7 @@ module.exports = function(config) {
     };
 
     middleware.hears = function(tests, message) {
-        if (message.entities.intent) {
+        if (message.entities && message.entities.intent) {
             for (var i = 0; i < message.entities.intent.length; i++) {
                 for (var t = 0; t < tests.length; t++) {
                     if (message.entities.intent[i].value == tests[t]) {
