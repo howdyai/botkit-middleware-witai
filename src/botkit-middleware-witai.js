f00f0779738f22b571a33975f9c5278e192c5534
koldkoder <kushal111@gmail.com>
2016-06-08 01:21:10 -0700

    Changes to support new wit api.

--- src/botkit-middleware-witai.js
+++ src/botkit-middleware-witai.js
@@ -1,4 +1,20 @@
-var wit = require('node-wit');
+var Logger = require('node-wit').Logger;
+var levels = require('node-wit').logLevels;
+var Wit = require('node-wit').Wit;
+
+
+var actions = {
+  say: function(sessionId, context, message, cb) {
+    console.log(message);
+    cb();
+  },
+  merge: function(sessionId, context, entities, message, cb) {
+    cb(context);
+  },
+  error: function(sessionId, context, error) {
+    console.log(error.message);
+  },
+};
 
 module.exports = function(config) {
 
@@ -6,22 +22,34 @@ module.exports = function(config) {
         throw new Error('No wit.ai API token specified');
     }
 
+    var logger = new Logger(levels.INFO);
+    var client = new Wit(config.token, actions, logger);
+
     if (!config.minimum_confidence) {
         config.minimum_confidence = 0.5;
     }
 
+    if (!config.intent_entity_name) {
+        config.intent_entity_name = 'intent'
+    }
+
     var middleware = {};
+    var context = {};
 
     middleware.receive = function(bot, message, next) {
         if (message.text) {
-            wit.captureTextIntent(config.token, message.text, function(err, res) {
-                if (err) {
-                    next(err);
+
+            client.message(message.text, context, function(error, data) {
+                if (error) {
+                    next(error);
                 } else {
-                    console.log(JSON.stringify(res));
-                    message.intents = res.outcomes;
+                    var entities = data.entities;
+                    if (entities) {
+                        message.entities = data.entities;
+                        message.intents = data.entities[config.intent_entity_name];
+                    }
                     next();
-                }
+                    }
             });
         }
 
@@ -32,7 +60,7 @@ module.exports = function(config) {
         if (message.intents) {
             for (var i = 0; i < message.intents.length; i++) {
                 for (var t = 0; t < tests.length; t++) {
-                    if (message.intents[i].intent == tests[t] &&
+                    if (message.intents[i].value == tests[t] &&
                         message.intents[i].confidence >= config.minimum_confidence) {
                         return true;
                     }
