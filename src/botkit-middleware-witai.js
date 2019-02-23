4aa3fd68d323555e88a8f59e4a5eb10e2e2895c0
Mario Buonomo <ma.buonomo@gmail.com>
2019-02-23 21:35:26 +0100

    load accessToken

--- src/botkit-middleware-witai.js
+++ src/botkit-middleware-witai.js
@@ -2,19 +2,19 @@ var Wit = require('node-wit').Wit;
 
 // not used at the moment
 var actions = {
-    say: function(sessionId, context, message, cb) {
+    say: function (sessionId, context, message, cb) {
         console.log(message);
         cb();
     },
-    merge: function(sessionId, context, entities, message, cb) {
+    merge: function (sessionId, context, entities, message, cb) {
         cb(context);
     },
-    error: function(sessionId, context, error) {
+    error: function (sessionId, context, error) {
         console.log(error.message);
     }
 };
 
-module.exports = function(config) {
+module.exports = function (config) {
 
     if (!config || !config.token) {
         throw new Error('No wit.ai API token specified');
@@ -24,15 +24,15 @@ module.exports = function(config) {
         config.minimum_confidence = 0.5;
     }
 
-    var client = new Wit(config.token, actions);
+    var client = new Wit({ accessToken: config.token, actions: actions });
 
     var middleware = {};
 
-    middleware.receive = function(bot, message, next) {
+    middleware.receive = function (bot, message, next) {
         // Only parse messages of type text and mention the bot.
         // Otherwise it would send every single message to wit (probably don't want that).
         if (message.text && message.text.indexOf(bot.identity.id) > -1) {
-            client.message(message.text, function(error, data) {
+            client.message(message.text, function (error, data) {
                 if (error) {
                     next(error);
                 } else {
@@ -48,7 +48,7 @@ module.exports = function(config) {
         }
     };
 
-    middleware.hears = function(tests, message) {
+    middleware.hears = function (tests, message) {
         if (message.entities && message.entities.intent) {
             for (var i = 0; i < message.entities.intent.length; i++) {
                 for (var t = 0; t < tests.length; t++) {
